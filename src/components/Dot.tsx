4026a55182a5195743c17a565a2bf378018d0a35
Rahel Lüthy <rahel.luethy@gmail.com>
2018-03-28 13:57:08 +0200

    Animate horizontal circle movement

--- src/components/Dot.tsx
+++ src/components/Dot.tsx
@@ -2,19 +2,49 @@ import { Item } from '../types';
 import * as React from 'react';
 import { scaleLinear } from 'd3-scale';
 import { stepCount } from '../constants';
+import Animate from 'react-move/Animate';
+import { easeExpOut } from 'd3-ease';
 
 export type Props = {
     readonly width: number;
     readonly item: Item;
 };
 
-const Dot = ({width, item}: Props) => {
+class Dot extends React.Component<Props> {
 
-    const scaleX = scaleLinear()
-        .domain([0, stepCount - 1])
-        .range([0, width]);
+    render() {
+        const {width, item} = this.props;
+        const scaleX = scaleLinear()
+            .domain([0, stepCount - 1])
+            .range([0, width]);
+        const cx = scaleX(item.step);
+        return (
+            <Animate
+                key={item.id}
 
-    return <circle r="1" cx={scaleX(item.step)} fill="steelblue"/>;
-};
+                start={{
+                    cx: cx
+                }}
+
+                update={{
+                    cx: [cx],
+                    timing: { duration: 1000, ease: easeExpOut },
+                }}
+
+            >
+                {(state) => {
+                    return (
+                        <circle
+                            r="1"
+                            cx={state.cx as number}
+                            fill="steelblue"
+                        />
+                    );
+                }}
+            </Animate>
+        );
+    }
+
+}
 
 export default Dot;
